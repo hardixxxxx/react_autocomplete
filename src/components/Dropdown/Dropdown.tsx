5a8f07941d5d8a26ba4ba34fcd4b97f384033f52
Anton Polishchuk <hardixxxxx@gmail.com>
2025-04-02 17:33:40 +0200

    Implement Autocomplete component using Bulma Dropdown

---
+++ src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,78 @@
+import classNames from 'classnames';
+import React, { useCallback, useState } from 'react';
+import debounce from 'lodash.debounce';
+import { Person } from '../../types/Person';
+
+type Props = {
+  people: Person[];
+  delay?: number;
+  setAppliedQuery: (query: string) => void;
+  onSelected: (person: Person | null) => void;
+};
+
+export const Dropdown: React.FC<Props> = React.memo(
+  ({ people, delay = 300, setAppliedQuery, onSelected }) => {
+    const [query, setQuery] = useState('');
+    const [isDropDown, setIsDropDown] = useState(false);
+
+    const applyQuery = useCallback(debounce(setAppliedQuery, delay), [delay]);
+
+    const handleChange = useCallback(
+      (event: React.ChangeEvent<HTMLInputElement>) => {
+        setQuery(event.target.value);
+        applyQuery(event.target.value.trim());
+        onSelected(null);
+      },
+      [],
+    );
+
+    const handleSelect = useCallback((person: Person) => {
+      setQuery(person.name);
+      setAppliedQuery(person.name);
+      onSelected(person);
+    }, []);
+
+    return (
+      <div className="dropdown is-active">
+        <div className="dropdown-trigger">
+          <input
+            type="text"
+            placeholder="Enter a part of the name"
+            className="input"
+            data-cy="search-input"
+            value={query}
+            onChange={handleChange}
+            onFocus={() => setIsDropDown(true)}
+            onBlur={() => setTimeout(() => setIsDropDown(false), delay)}
+          />
+        </div>
+
+        {isDropDown && people.length > 0 && (
+          <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
+            <div className="dropdown-content">
+              {people.map(person => (
+                <div
+                  key={person.slug}
+                  className="dropdown-item"
+                  data-cy="suggestion-item"
+                  onClick={() => handleSelect(person)}
+                >
+                  <p
+                    className={classNames({
+                      'has-text-link': person.sex === 'm',
+                      'has-text-danger': person.sex === 'f',
+                    })}
+                  >
+                    {person.name}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  },
+);
+
+Dropdown.displayName = 'Dropdown';
